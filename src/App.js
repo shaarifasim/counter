6f92d4a9913c245aa78d34e17cfbc982b708397b
zaidasim2197 <https://github.com/zaidasim2197/github-link-in-cloud.git>
2022-10-17 23:06:38 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
+import {useState} from "react";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button  className="Add" onClick="Add">Add</button>
+      <div className="screen"> 
+      <h2>"Counter"</h2>
+      </div>
+      <button  className="Add" onClick="Subtract">Subtract</button>
     </div>
   );
 }
